3e9360b7f291d8636aecfe73e5eb2d0805db4918
tsiutsiu <amazingeleanna@gmail.com>
2019-09-20 09:49:17 +0100

    Add fix to LastInsertIDReturningSuffix()

--- dialects/mssql/mssql.go
+++ dialects/mssql/mssql.go
@@ -191,7 +191,7 @@ func (mssql) SelectFromDummyTable() string {
 }
 
 func (mssql) LastInsertIDReturningSuffix(tableName, columnName string) string {
-	return ""
+	return "SELECT SCOPE_IDENTITY()"
 }
 
 func (mssql) DefaultValueStr() string {
